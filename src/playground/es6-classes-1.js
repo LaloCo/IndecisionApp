2aa6f0385a762b6693f7b29bc64cab5b88f333f3
Eduardo Rosas <lalorosas@outlook.com>
2019-05-09 17:38:46 -0500

    More class practice

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -1,16 +1,23 @@
 
 class Person {
-    constructor(name = 'Anonymous') {
+    constructor(name = 'Anonymous', age = 0) {
         this.name = name;
+        this.age = age;
     }
 
     getGreeting() {
         return `Hello, I am ${this.name}!`;
     }
+
+    getDescription() {
+        return `${this.name} is ${this.age} year(s) old`;
+    }
 }
 
-const me = new Person('Eduardo Rosas');
+const me = new Person('Eduardo Rosas', 26);
 console.log(me.getGreeting());
+console.log(me.getDescription());
 
 const other = new Person();
 console.log(other.getGreeting());
+console.log(other.getDescription());
