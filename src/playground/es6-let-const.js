7b3272fb25f75a4af4c6a225e78775c5bd1c303e
Eduardo Rosas <lalorosas@outlook.com>
2019-05-09 08:49:28 -0500

    Add JS playground

--- src/playground/es6-let-const.js
+++ src/playground/es6-let-const.js
@@ -1,12 +1,10 @@
-'use strict';
-
 var nameVar = 'Eduardo';
 var nameVar = 'Yesenia'; //with var we can redefine, so we could mistakenly do so and not have access to the first one anymore.
 console.log('nameVar', nameVar);
 
-var nameLet = 'Eduardo';
+let nameLet = 'Eduardo';
 nameLet = 'Yesenia'; // with let we cannot redefine, so there is no problem.
 console.log('nameLet', nameLet);
 
-var nameConst = 'Eduardo'; // Like with let, we cannot redefine it, but neither can we reassign
+const nameConst = 'Eduardo'; // Like with let, we cannot redefine it, but neither can we reassign
 console.log('nameConst', nameConst);
