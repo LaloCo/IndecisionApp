65a9f7af886d28de15e3eb9b3965621323ae470f
Eduardo Rosas <lalorosas@outlook.com>
2019-05-09 14:59:21 -0500

    List options

--- src/app.js
+++ src/app.js
@@ -31,7 +31,11 @@ const render = () => {
             {app.subtitle && <p>{app.subtitle}</p>}
             <button onClick={clearOptions}>Remove all</button>
             <p>{app.options.length > 0 ? 'Here are your options:' : 'No options'}</p>
-            <p>{app.options.length}</p>
+            <ol>
+            {
+                app.options.map((option) => <li>{option}</li>)
+            }
+            </ol>
             <form onSubmit={addOption}>
                 <input type="text" name="optionText"/>
                 <button>Add option</button>
