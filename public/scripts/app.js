3155fe09960bb8e282a85ca4cd8e2cf0504efa64
Eduardo Rosas <lalorosas@outlook.com>
2019-05-08 20:08:31 -0500

    True conditions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,31 +1,10 @@
 'use strict';
 
-console.log('App.js is running!');
-
 var app = {
     title: 'Indecision App',
     subtitle: 'Put your life in the hands of a computer',
-    age: 17
+    options: ['one', 'two']
 };
-
-function getSomething() {
-    if (new Date().getMonth() > 6) {
-        return "You are not on track";
-    }
-    return "You are on track";
-}
-
-function showSignInButton(show) {
-    if (show) {
-        return React.createElement(
-            'button',
-            null,
-            'Inicia sesi\xF3n'
-        );
-    } else {
-        return undefined;
-    }
-}
 
 // JSX - JavaScript XML
 var template = React.createElement(
@@ -36,24 +15,17 @@ var template = React.createElement(
         null,
         app.title
     ),
-    React.createElement(
+    app.subtitle && React.createElement(
         'p',
         null,
-        app.subtitle ? app.subtitle : 'Well, this is unexpected'
+        'app.subtitle'
     ),
     React.createElement(
         'p',
         null,
-        getSomething()
-    ),
-    showSignInButton(true),
-    app.age && app.age >= 18 && React.createElement(
-        'p',
-        null,
-        'Age: ',
-        app.age
+        app.options.length > 0 ? 'Here are your options:' : 'No options:'
     )
 );
-var appRoot = document.getElementById('app');
 
+var appRoot = document.getElementById('app');
 ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,36 +1,17 @@
-console.log('App.js is running!');
-
 var app = {
     title: 'Indecision App',
     subtitle: 'Put your life in the hands of a computer',
-    age: 17,
+    options: ['one', 'two'],
 };
-
-function getSomething(){
-    if((new Date()).getMonth() > 6 ) {
-        return "You are not on track"
-    }
-    return "You are on track"
-}
-
-function showSignInButton(show){
-    if(show) {
-        return <button>Inicia sesión</button>
-    } else {
-        return undefined
-    }
-}
 
 // JSX - JavaScript XML
 var template  = (
     <div>
         <h1>{app.title}</h1>
-        <p>{app.subtitle ? app.subtitle : 'Well, this is unexpected'}</p>
-        <p>{getSomething()}</p>
-        {showSignInButton(true)}
-        {(app.age && app.age >= 18) && <p>Age: {app.age}</p>}
+        {app.subtitle && <p>app.subtitle</p>}
+        <p>{app.options.length > 0 ? 'Here are your options:' : 'No options:'}</p>
     </div>
 );
-var appRoot = document.getElementById('app');
 
+var appRoot = document.getElementById('app');
 ReactDOM.render(template, appRoot);
