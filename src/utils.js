06989c241396eb0628a7b4cf6220f76ce0767db6
Eduardo Rosas <lalorosas@outlook.com>
2019-05-17 13:58:52 -0500

    Import/export practice

---
+++ src/utils.js
@@ -0,0 +1,7 @@
+console.log('utils.js is running.');
+
+export const square = (x) => x * x;
+
+//? This is the alternative if we don't want to write export on
+//? every value we want to export.
+//* export { square };
